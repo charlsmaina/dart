feac175de38b4eadfd6f85cbf4461f0a7ab5b0af
charlsmaina <mainacharles1496@gmail.com>
2026-03-17 08:59:13 +0300

    records

---
+++ dart_primer/records.dart
@@ -0,0 +1,15 @@
+
+void main()
+{
+    /*
+    Records - a way to store together related data of differenrt types 
+    - Analogous to structs in c
+    - they two ways to store: store separating with commas - access using index ..ie $index
+    - named storage - use name:value - access by name
+
+    
+     */
+
+     var student = (name:"Charles Maina",age:23);
+     print(student.age);
+}
